3b8a30b88fc20563ce6a39f4f0c5cad97cdc247e
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-11-04 19:22:39 +0600

    pages added

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar/>
             <div>
                 <Outlet/>
             </div>
